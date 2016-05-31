9d041b6447cc06e9e297282be12e850ba5779ef3
JordanBurnett1210 <jabur1210@gmail.com>
2016-05-31 09:13:42 -0600

    Added a fireball

--- Sample/Controller/Game1.cs
+++ Sample/Controller/Game1.cs
@@ -7,9 +7,7 @@ using Microsoft.Xna.Framework.Input;
 
 namespace Sample.Controller
 {
-	/// <summary>
-	/// This is the main type for your game.
-	/// </summary>
+	
 	public class Game1 : Game
 	{
 		GraphicsDeviceManager graphics;
@@ -21,12 +19,7 @@ namespace Sample.Controller
 			Content.RootDirectory = "Content";
 		}
 
-		/// <summary>
-		/// Allows the game to perform any initialization it needs to before starting to run.
-		/// This is where it can query for any required services and load any non-graphic
-		/// related content.  Calling base.Initialize will enumerate through any components
-		/// and initialize them as well.
-		/// </summary>
+
 		protected override void Initialize ()
 		{
 			// TODO: Add your initialization logic here
@@ -34,10 +27,7 @@ namespace Sample.Controller
 			base.Initialize ();
 		}
 
-		/// <summary>
-		/// LoadContent will be called once per game and is the place to load
-		/// all of your content.
-		/// </summary>
+
 		protected override void LoadContent ()
 		{
 			// Create a new SpriteBatch, which can be used to draw textures.
@@ -46,11 +36,7 @@ namespace Sample.Controller
 			//TODO: use this.Content to load your game content here 
 		}
 
-		/// <summary>
-		/// Allows the game to run logic such as updating the world,
-		/// checking for collisions, gathering input, and playing audio.
-		/// </summary>
-		/// <param name="gameTime">Provides a snapshot of timing values.</param>
+
 		protected override void Update (GameTime gameTime)
 		{
 			// For Mobile devices, this logic will close the Game when the Back button is pressed
@@ -65,10 +51,7 @@ namespace Sample.Controller
 			base.Update (gameTime);
 		}
 
-		/// <summary>
-		/// This is called when the game should draw itself.
-		/// </summary>
-		/// <param name="gameTime">Provides a snapshot of timing values.</param>
+
 		protected override void Draw (GameTime gameTime)
 		{
 			graphics.GraphicsDevice.Clear (Color.CornflowerBlue);
