f9d952be557aa250e9100e4069f7519855f96d44
JordanBurnett1210 <jabur1210@gmail.com>
2016-06-02 08:13:08 -0600

    Added a lot of updating

--- Sample/Controller/Game1.cs
+++ Sample/Controller/Game1.cs
@@ -4,12 +4,32 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Storage;
 using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Input.Touch;
 
 namespace Sample.Controller
 {
 	
 	public class Game1 : Game
 	{
+		// Image used to display the static background
+		Texture2D mainBackground;
+
+		// Parallaxing Layers
+		ParallaxingBackground bgLayer1;
+		ParallaxingBackground bgLayer2;
+		// Keyboard states used to determine key presses
+		KeyboardState currentKeyboardState;
+		KeyboardState previousKeyboardState;
+
+		// Gamepad states used to determine button presses
+		GamePadState currentGamePadState;
+		GamePadState previousGamePadState; 
+
+		// A movement speed for the player
+		float playerMoveSpeed;
+		// Represents the player 
+		Player player;
+
 		GraphicsDeviceManager graphics;
 		SpriteBatch spriteBatch;
 
@@ -22,7 +42,16 @@ namespace Sample.Controller
 
 		protected override void Initialize ()
 		{
+			// Initialize the player class
+			player = new Player();
+			// Set a constant player move speed
+			playerMoveSpeed = 8.0f;
+
+			//Enable the FreeDrag gesture.
+			TouchPanel.EnabledGestures = GestureType.FreeDrag;
 			// TODO: Add your initialization logic here
+			bgLayer1 = new ParallaxingBackground();
+			bgLayer2 = new ParallaxingBackground();
             
 			base.Initialize ();
 		}
@@ -32,13 +61,28 @@ namespace Sample.Controller
 		{
 			// Create a new SpriteBatch, which can be used to draw textures.
 			spriteBatch = new SpriteBatch (GraphicsDevice);
+			// Load the player resources 
+			Vector2 playerPosition = new Vector2(GraphicsDevice.Viewport.TitleSafeArea.X,GraphicsDevice.Viewport.TitleSafeArea.Y +GraphicsDevice.Viewport.TitleSafeArea.Height / 2);
+			// Load the player resources
+			Animation playerAnimation = new Animation();
+			Texture2D playerTexture = Content.Load<Texture2D>("shipAnimation");
+			playerAnimation.Initialize(playerTexture, Vector2.Zero, 115, 69, 8, 30, Color.White, 1f, true);
 
+			Vector2 playerPosition = new Vector2 (GraphicsDevice.Viewport.TitleSafeArea.X, GraphicsDevice.Viewport.TitleSafeArea.Y
+				+ GraphicsDevice.Viewport.TitleSafeArea.Height / 2);
+			player.Initialize(playerAnimation, playerPosition);
 			//TODO: use this.Content to load your game content here 
+			// Load the parallaxing background
+			bgLayer1.Initialize(Content, "bgLayer1", GraphicsDevice.Viewport.Width, -1);
+			bgLayer2.Initialize(Content, "bgLayer2", GraphicsDevice.Viewport.Width, -2);
+
+			mainBackground = Content.Load<Texture2D>("mainbackground");
 		}
 
 
 		protected override void Update (GameTime gameTime)
 		{
+			
 			// For Mobile devices, this logic will close the Game when the Back button is pressed
 			// Exit() is obsolete on iOS
 			#if !__IOS__ &&  !__TVOS__
@@ -46,14 +90,71 @@ namespace Sample.Controller
 				Exit ();
 			#endif
             
-			// TODO: Add your update logic here
+			// Save the previous state of the keyboard and game pad so we can determinesingle key/button presses
+			previousGamePadState = currentGamePadState;
+			previousKeyboardState = currentKeyboardState;
+
+			// Read the current state of the keyboard and gamepad and store it
+			currentKeyboardState = Keyboard.GetState();
+			currentGamePadState = GamePad.GetState(PlayerIndex.One);
+			//Update the player
+			UpdatePlayer(gameTime);
+			// Update the parallaxing background
+			bgLayer1.Update();
+			bgLayer2.Update();
             
 			base.Update (gameTime);
+		}
+
+		private void UpdatePlayer(GameTime gameTime)
+		{
+			player.Update(gameTime);
+			// Get Thumbstick Controls
+			player.Position.X += currentGamePadState.ThumbSticks.Left.X *playerMoveSpeed;
+			player.Position.Y -= currentGamePadState.ThumbSticks.Left.Y *playerMoveSpeed;
+
+			// Use the Keyboard / Dpad
+			if (currentKeyboardState.IsKeyDown(Keys.Left) ||
+				currentGamePadState.DPad.Left == ButtonState.Pressed)
+			{
+				player.Position.X -= playerMoveSpeed;
+			}
+			if (currentKeyboardState.IsKeyDown(Keys.Right) ||
+				currentGamePadState.DPad.Right == ButtonState.Pressed)
+			{
+				player.Position.X += playerMoveSpeed;
+			}
+			if (currentKeyboardState.IsKeyDown(Keys.Up) ||
+				currentGamePadState.DPad.Up == ButtonState.Pressed)
+			{
+				player.Position.Y -= playerMoveSpeed;
+			}
+			if (currentKeyboardState.IsKeyDown(Keys.Down) ||
+				currentGamePadState.DPad.Down == ButtonState.Pressed)
+			{
+				player.Position.Y += playerMoveSpeed;
+			}
+
+			// Make sure that the player does not go out of bounds
+			player.Position.X = MathHelper.Clamp(player.Position.X, 0,GraphicsDevice.Viewport.Width - player.Width);
+			player.Position.Y = MathHelper.Clamp(player.Position.Y, 0,GraphicsDevice.Viewport.Height - player.Height);
 		}
 
 
 		protected override void Draw (GameTime gameTime)
 		{
+			// Start drawing
+			spriteBatch.Begin();
+			spriteBatch.Draw(mainBackground, Vector2.Zero, Color.White);
+
+			// Draw the moving background
+			bgLayer1.Draw(spriteBatch);
+			bgLayer2.Draw(spriteBatch);
+			// Draw the Player
+			player.Draw(spriteBatch);
+
+			// Stop drawing
+			spriteBatch.End();
 			graphics.GraphicsDevice.Clear (Color.CornflowerBlue);
             
 			//TODO: Add your drawing code here
